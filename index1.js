f47bd95ebba02c2df6a51f34fe151f844383551a
Essence Alexander <upperlefty@zoho.eu>
2018-01-11 16:47:32 +0200

    Update index1.js

--- index1.js
+++ index1.js
@@ -232,7 +232,7 @@ Game.Draw = function(context,snake) {
 this.drawCell = function(x, y)  {
   context.fillStyle = 'rgb(170, 170, 170)';
   context.beginPath();
-  context.arc((x * snake.stage.conf.cw + 6), (y * snake.stage.conf.cw + 6),
+  context.arc((x * snake.stage.conf.cw + 6), (y * snake.stage.conf.cw + 6), 4, 0, 2*Math.PI, false);
   
   
   
